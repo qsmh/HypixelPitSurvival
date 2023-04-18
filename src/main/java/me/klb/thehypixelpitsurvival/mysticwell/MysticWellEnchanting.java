5b849d4f564a785604d91b9c903305883cb84d4d
BuildTools <unconfigured@null.spigotmc.org>
2023-04-18 07:04:44 -0400

    Changed y position of Item Enchants hologram to make it more accurate.

--- src/main/java/me/klb/thehypixelpitsurvival/mysticwell/MysticWellEnchanting.java
+++ src/main/java/me/klb/thehypixelpitsurvival/mysticwell/MysticWellEnchanting.java
@@ -2,28 +2,80 @@ package me.klb.thehypixelpitsurvival.mysticwell;
 
 import me.klb.thehypixelpitsurvival.TheHypixelPitSurvival;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MysticWellEnchanting implements Listener {
+
+    public static Object[] calculateEnchanting(ItemStack currentItem, Player player){
+        Integer goldCost = null;
+        String itemTier = null;
+        String canAfford = "true";
+
+        ItemMeta currentItemMeta = currentItem.getItemMeta();
+        assert currentItemMeta != null;
+
+        PersistentDataContainer currentItemData = currentItemMeta.getPersistentDataContainer();
+
+        if (!currentItemData.has(new NamespacedKey(TheHypixelPitSurvival.getPlugin(), "MysticItem"), PersistentDataType.STRING)){
+            return null;
+        }
+
+        String mysticItemData = currentItemData.get(new NamespacedKey(TheHypixelPitSurvival.getPlugin(), "MysticItem"), PersistentDataType.STRING);
+
+        switch (mysticItemData){ // Ignore highlighted stuff here
+            case "Fresh":
+                goldCost = 1000;
+                itemTier = "I";
+                break;
+            case "Fresh 1":
+                goldCost = 4000;
+                itemTier = "II";
+                break;
+            case "Fresh 2":
+                goldCost = 8000;
+                itemTier = "III";
+                break;
+        }
+
+        // Continue here if player has something special that takes off some price for the gold
+
+        PersistentDataContainer data = player.getPersistentDataContainer();
+        Double goldBalance = data.get(new NamespacedKey(TheHypixelPitSurvival.getPlugin(), "GoldBalance"), PersistentDataType.DOUBLE);
+
+        if (goldCost > goldBalance){
+            canAfford = "false";
+        }
+
+        return new Object[]{goldCost, itemTier, canAfford};
+    }
     public static void createMysticWellInventory(Player player){
         Inventory inventory = Bukkit.createInventory(player, 45, "Mystic Well");
 
         ItemStack mysticWell = new ItemStack(Material.ENCHANTING_TABLE);
         ItemMeta mysticWellMeta = mysticWell.getItemMeta();
-
         assert mysticWellMeta != null;
 
+        PersistentDataContainer mysticWellData = mysticWellMeta.getPersistentDataContainer();
+        mysticWellData.set(new NamespacedKey(TheHypixelPitSurvival.getPlugin(), "MysticWellEnchanting"), PersistentDataType.STRING, "true");
+
         mysticWellMeta.setDisplayName(TheHypixelPitSurvival.getPlugin().getConfig().getString("mystic-well-displayname"));
+        mysticWell.setItemMeta(mysticWellMeta);
 
         inventory.setItem(24, mysticWell);
 
@@ -37,6 +89,19 @@ public class MysticWellEnchanting implements Listener {
         inventory.setItem(30, new ItemStack(Material.GRAY_STAINED_GLASS_PANE));
 
         player.openInventory(inventory);
+    }
+
+    @EventHandler
+    public void onMysticWellInventoryClose(InventoryCloseEvent event){
+        Inventory inventory = event.getInventory();
+        Player player = (Player) event.getPlayer();
+
+        if (inventory.getItem(20) == null){
+            return;
+        }
+
+        ItemStack item = inventory.getItem(20);
+        player.getInventory().addItem(item);
     }
 
     @EventHandler
@@ -59,11 +124,11 @@ public class MysticWellEnchanting implements Listener {
                         player.getInventory().addItem(currentItem);
                         inventory.setItem(20, null);
                         player.updateInventory();
-                        return;
                     }else{
                         event.setCancelled(true);
-                        return;
                     }
+
+                    return;
                 }
             }
 
@@ -83,7 +148,33 @@ public class MysticWellEnchanting implements Listener {
                 player.updateInventory();
             }else{
                 event.setCancelled(true);
+                return;
             }
+
+            Object[] result = calculateEnchanting(currentItem, player);
+
+            if (result == null){ // This never should happen
+                return;
+            }
+
+            Integer goldCost = (Integer) result[0];
+            String itemTier = (String) result[0]; // Ignore highlighted
+
+            ItemStack mysticWell = inventory.getItem(24);
+
+            if (mysticWell == null){ // Also should never happen
+                return;
+            }
+
+            ItemMeta mysticWellMeta = mysticWell.getItemMeta();
+            List<String> lore = new ArrayList<>();
+            DecimalFormat costFormat = new DecimalFormat("#,###");
+
+
+            lore.add(ChatColor.GRAY + "Upgrade: " + ChatColor.GREEN + "Tier " + itemTier);
+            lore.add(ChatColor.GRAY + "Cost: " + ChatColor.GOLD + costFormat.format(goldCost.toString()) + "g");
+
+
         }
     }
 }
